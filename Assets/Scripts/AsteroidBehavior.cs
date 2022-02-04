b9f09571f114dbf4a15206ddcb20a0284f2e2a34
jbthelt <jbarthelt@mail.bradley.edu>
2022-02-03 18:18:25 -0600

    Asteroid destruction

--- Assets/Scripts/AsteroidBehavior.cs
+++ Assets/Scripts/AsteroidBehavior.cs
@@ -14,19 +14,26 @@ public class AsteroidBehavior : MonoBehaviour
 {
 
     ThirdPersonController tpc;
-  
+
+    IDamagable damagable;
+
+    Rigidbody rb;
+    public AudioSource aSource;
+    public AudioClip destroy;
 
     // Start is called before the first frame update
     void Start()
     {
         tpc = GameObject.Find("Player").GetComponent<ThirdPersonController>();
-       
+        rb = GetComponent<Rigidbody>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        
         
     }
 
@@ -34,11 +41,25 @@ public class AsteroidBehavior : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(this.gameObject);
+            aSource.clip = destroy;
+            aSource.Play();
+            Destroy(this.gameObject, 0.2f);
             print("hit player");
             tpc.health -= 5;
             tpc.UpdateHealthBar();
             
         }
+        if (collision.gameObject.tag == "asteroid")
+        {
+            //aSource.clip = destroy;
+            //aSource.Play();
+            Destroy(this.gameObject, 0.2f);
+            Destroy(collision.gameObject, 0.2f);
+
+        }
+
+
     }
+
+
 }
