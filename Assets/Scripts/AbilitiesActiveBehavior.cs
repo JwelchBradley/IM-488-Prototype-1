9a57fc6f7d037dee53cc642638c8f4195c98654d
jbthelt <jbarthelt@mail.bradley.edu>
2022-02-13 11:49:04 -0600

    UI updates to match ability select

--- Assets/Scripts/AbilitiesActiveBehavior.cs
+++ Assets/Scripts/AbilitiesActiveBehavior.cs
@@ -8,7 +8,11 @@ public class AbilitiesActiveBehavior : MonoBehaviour
     SelectionBehavior sb;
     GameObject aManager;
     GameObject selectControl;
-    
+
+    public GameObject ab1;
+    public GameObject ab2;
+    public GameObject ab3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +23,29 @@ public class AbilitiesActiveBehavior : MonoBehaviour
         if(sb.ability1 != "gravity" && sb.ability2 != "gravity")
         {
             aManager.GetComponent<GravityPull>().enabled = false;
+            ab1.SetActive(false);
+        }
+        else
+        {
+            ab1.SetActive(true);
         }
         if (sb.ability1 != "EMP" && sb.ability2 != "EMP")
         {
             aManager.GetComponent<FreezeEnemy>().enabled = false;
+            ab2.SetActive(false);
+        }
+        else
+        {
+            ab2.SetActive(true);
         }
         if (sb.ability1 != "shield" && sb.ability2 != "shield")
         {
             aManager.GetComponent<Shield>().enabled = false;
+            ab3.SetActive(false);
+        }
+        else
+        {
+            ab3.SetActive(true);
         }
 
     }
